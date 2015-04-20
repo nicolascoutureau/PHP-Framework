9d4a5eb0b9ae5447614397be045ca57c095fc75a
Sharewood-imac <sharewood@iMac-de-Sharewood-imac.local>
2015-04-20 17:01:26 +0200

    autoload

---
+++ app/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sharewood
+ * Date: 20/04/15
+ * Time: 16:50
+ */
+namespace Project\app;
+
+class test {
+
+} 
---
+++ public/index.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sharewood
+ * Date: 20/04/15
+ * Time: 16:41
+ */
+
+require_once '../vendor/autoload.php';
+
+new \Project\app\test();
+
+echo 'ok';
